3bcdfb541ee7845c8a23c3b547aca3b24508bb36
Luc <34852@ma-web.nl>
2023-01-12 14:18:08 +0100

    fucked

--- game/js/Game.js
+++ game/js/Game.js
@@ -11,3 +11,29 @@ ctx.lineTo(-30,40);
 ctx.lineTo(70,20);
 ctx.stroke();
 ctx.fill();  
+console.log("hello")
+
+while(true){
+    
+}
+
+//tekenposities
+let x = 100;
+let y = 100;
+let size = 10;
+//kleur
+ctx.fillStyle = "red";
+function update(time){
+    //beweging
+    x++;
+    //teken vierkant
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+    ctx.lineTo(x+size,y);
+    ctx.lineTo(x+size,y+size);
+    ctx.lineTo(x,y+size);
+    ctx.lineTo(x,y);
+    ctx.fill();
+    window.requestAnimationFrame(update);
+}
+window.requestAnimationFrame(update);
